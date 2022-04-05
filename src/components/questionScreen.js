11ccfe1538a018a871dc01eb2e833f5a379a8470
Itay Levy <levyetay@gmail.com>
2022-04-05 21:50:44 +0300

    added QuestionScreen component

---
+++ src/components/questionScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Player from './player';
+import Score from './score';
+import Question from './question';
+import AnswerButtom from './answerButton';
+
+class QuestionScreen extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        <div>
+        <Question question={this.props.question}/>
+        <Player name={this.props.name} />
+        <Score points={this.props.points} />
+        <AnswerButtom isCorrect='true' answer={this.props.answer} />   
+        </div>
+    }
+}
+export default QuestionScreen;
